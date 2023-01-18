7acc3557d56a1ee199cc74a901ccdbacfd5b3da4
overmonster27 <overmonster27@gmail.com>
2023-01-18 21:18:44 +0200

    'lesson'

---
+++ src/Components/Users/User.js
@@ -0,0 +1,12 @@
+const User = (user) => {
+    const {id, name} = User;
+
+    return (
+        <div>
+            <div>Id: {id}</div>
+            <div>Name: {name}</div>
+        </div>
+    );
+};
+
+export {User};

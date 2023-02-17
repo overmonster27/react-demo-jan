fddd539e940a944a258a48d5d204445d10ef14ef
overmonster27 <overmonster27@gmail.com>
2023-02-17 13:28:23 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {FC} from "react";
+import {Cars} from "./components";
+import './App.css'
+
+const App: FC = () => {
+    return (
+        <div>
+            <Cars/>
+        </div>
+    );
+};
+
+export {App};
